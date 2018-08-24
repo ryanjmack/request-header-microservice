ce0756a5613eee939e2a657b86cff25b22c4668f
root <ryanjmack21@gmail.com>
2018-08-24 18:20:01 -0400

    Serve index.html

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-
+const PORT    = Number(process.env.PORT || 3000);
 const express = require('express');
 const app     = express();
 
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', (req, res) => {
-  res.send('Testing!');
+  res.sendFile('index.html');
 });
 
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
